3873d7bd009d4e189b85deed88471b1f45f6d1cc
b2pkick <rahulgupta79777@gmail.com>
2025-06-21 01:17:18 +0530

    done with the working of api and pokemon data

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,35 @@
-import { useEffect, useState } from 'react'
-import {PokemonCard} from "./components/PokemonCard"
+import { useEffect, useState} from 'react'
+// import {PokemonCard} from "./components/PokemonCard"
 
 function App() {
+  const [pokemon,setPokemon] = useState([])
 
+
+  const API = "https://pokeapi.co/api/v2/pokemon?limit=24"
   useEffect(()=>{
-    
+    const pokemonData = async function(){
+      let res = await fetch(API)
+      let data = await res.json()
+      const pokemonDataUrl = data.results.map(async (curr)=>{
+        res = await fetch(curr.url)
+        data = await res.json()
+        return data
+      })
+      const pokemonFinalData = await Promise.all(pokemonDataUrl)
+      setPokemon(pokemonFinalData)
+      console.log(pokemonFinalData)
+    }
+    pokemonData()
   },[])
 
+
   return (
-    <>
-      
-    </>
+    <div className='w-full min-h-screen flex flex-col items-center justify-center'>
+      <h1>Pokemon Cards</h1>
+      {pokemon.map(curr=>{
+        return <p key={curr.id}>{curr.name}</p>
+      })}
+    </div>
   )
 }
 
